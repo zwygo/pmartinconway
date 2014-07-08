a42cdf25f9ab5ca353d6439bb52427d6555b5d7e
Zach <zach@piazza.com>
2014-07-07 20:57:38 -0700

    initial code commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pmartinconway::Application.config.secret_key_base = '6bab5d844dd2354c019281fe48c1e73a46ed584027bf41a8fdaedf9f819ed4163fdcf4c215522c3078431167406ff6a5bfe565bb4912a66643184b7a6c6178cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pmartinconway::Application.config.session_store :cookie_store, key: '_pmartinconway_session'
